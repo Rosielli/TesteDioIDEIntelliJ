fd28aa8840955349b91de7abed96c2e9c774e4c9
Rosielli <rosielliandrade@outlook.com>
2022-07-16 18:29:02 -0300

    first commit

---
+++ src/DioMe/Calculadora/Calculadora.java
@@ -0,0 +1,36 @@
+package DioMe.Calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        int a, b;
+
+        System.out.println("Digite o primeiro valor: ");
+        a = entrada.nextInt();
+        System.out.println("Digite o segundo valor: ");
+        b = entrada.nextInt();
+
+        int somar = somar(a, b);
+        int subtrair = subtrair(a, b);
+        int multiplicar = multiplicar(a, b);
+        int dividir = dividir(a, b);
+
+        System.out.println("somar: " + somar);
+        System.out.println("subtrair: " + subtrair);
+        System.out.println("multiplicar: " + multiplicar);
+        System.out.println("dividir: " + dividir);
+    }
+
+    public static int somar (int a, int b) {
+        return a + b;
+    } public static int subtrair (int a, int b) {
+        return a - b;
+    } public static int multiplicar (int a, int b) {
+        return a * b;
+    } public static int dividir (int a, int b) {
+        return a / b;
+    }
+}
